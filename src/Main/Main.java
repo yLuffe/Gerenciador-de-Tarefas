3329f4541881488f7a6e1def3b9226d14129daf7
Luiz <yluffee@gmail.com>
2023-08-23 13:22:23 -0300

    Database

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,14 @@
 package Main;
+import Database.ConexaoSQLite;
 import Model.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        
+        ConexaoSQLite conexaoSQLite = new ConexaoSQLite();
+        conexaoSQLite.conectar();
+        conexaoSQLite.desconectar();
         
         Tarefa task = new Tarefa();
         
